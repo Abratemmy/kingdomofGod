6441d0a9ec3a03c71082ab56e3bb5c9210cde62e
temitope <temitope@aitechma.com>
2020-06-18 16:46:51 -0800

    Updates

---
+++ precache-manifest.d6d4e332c41945e01e412274d7cfdab4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455434a63c944843e804c4ec091385f4",
+    "url": "/kingdomofGod/index.html"
+  },
+  {
+    "revision": "7b8b18dbb542a899c2a9",
+    "url": "/kingdomofGod/static/css/2.c1e1db11.chunk.css"
+  },
+  {
+    "revision": "b7ecbbfad7f8a375782a",
+    "url": "/kingdomofGod/static/css/main.35f24b9d.chunk.css"
+  },
+  {
+    "revision": "454038523d7c703ee9b2",
+    "url": "/kingdomofGod/static/js/10.8c931f8b.chunk.js"
+  },
+  {
+    "revision": "35fd1d77e39e262fca91",
+    "url": "/kingdomofGod/static/js/11.7a187d7b.chunk.js"
+  },
+  {
+    "revision": "3364506d8dba70d57ce4",
+    "url": "/kingdomofGod/static/js/12.ac87e9ad.chunk.js"
+  },
+  {
+    "revision": "3a10342376f21dc47f4a",
+    "url": "/kingdomofGod/static/js/13.38a2791b.chunk.js"
+  },
+  {
+    "revision": "62545a87aab42a60ed97",
+    "url": "/kingdomofGod/static/js/14.7f3fdf6f.chunk.js"
+  },
+  {
+    "revision": "7b8b18dbb542a899c2a9",
+    "url": "/kingdomofGod/static/js/2.9c6b6a5c.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/kingdomofGod/static/js/2.9c6b6a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1118042b1a5c13a148f",
+    "url": "/kingdomofGod/static/js/3.5353e0d2.chunk.js"
+  },
+  {
+    "revision": "a14f141b7af91486073d",
+    "url": "/kingdomofGod/static/js/4.e2b4ad00.chunk.js"
+  },
+  {
+    "revision": "dc7833d02fa251bffe8f",
+    "url": "/kingdomofGod/static/js/5.c05aab4e.chunk.js"
+  },
+  {
+    "revision": "7c6d9597ed242ab8d5aa",
+    "url": "/kingdomofGod/static/js/6.922b5a90.chunk.js"
+  },
+  {
+    "revision": "ce8bb78703e89a77da44",
+    "url": "/kingdomofGod/static/js/7.b6959a27.chunk.js"
+  },
+  {
+    "revision": "9102c1425e2594632b83",
+    "url": "/kingdomofGod/static/js/8.1f975d96.chunk.js"
+  },
+  {
+    "revision": "c03ef8525293d4f515ce",
+    "url": "/kingdomofGod/static/js/9.72098200.chunk.js"
+  },
+  {
+    "revision": "b7ecbbfad7f8a375782a",
+    "url": "/kingdomofGod/static/js/main.0ef381e9.chunk.js"
+  },
+  {
+    "revision": "7661e923bb2c2177348f",
+    "url": "/kingdomofGod/static/js/runtime-main.0aa6a73f.js"
+  },
+  {
+    "revision": "e89a6fac5f6658cfdf8f5b7362db12be",
+    "url": "/kingdomofGod/static/media/donation.e89a6fac.PNG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/kingdomofGod/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/kingdomofGod/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/kingdomofGod/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/kingdomofGod/static/media/slick.f97e3bbf.svg"
+  }
+]);
